aeb78c65998419db2c408e3e35046a783732f4dc
Miguel Ferreira <mpaf98@gmail.com>
2021-12-13 14:46:58 +0200

    commit missing handler

---
+++ CDKPipelineApp/lib/lambda/handler.js
@@ -0,0 +1,8 @@
+
+exports.handler = async function(event) {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/json" },
+    body: JSON.stringify({ message: "Hello from my Lambda node!" })
+  };
+};
