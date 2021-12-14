f5fd446e95d698c4b5193daedf4dfc902e207c22
Miguel Ferreira <mpaf98@gmail.com>
2021-12-14 14:36:17 +0200

    add a post stackstep

--- CDKPipelineApp/lib/cdk_pipeline_app-stack.ts
+++ CDKPipelineApp/lib/cdk_pipeline_app-stack.ts
@@ -72,6 +72,10 @@ export class CdkPipelineAppStack extends Stack {
       stackSteps: [{
         stack: prodStage.appStack,
         changeSet: [new pipelines.ManualApprovalStep('ChangeSet Approval')],
+        post: [new pipelines.ShellStep('Validate Endpoint',
+        {
+          commands: ['curl -Ssf https://my.webservice.com/']
+        })]
       }],
     });
   }
